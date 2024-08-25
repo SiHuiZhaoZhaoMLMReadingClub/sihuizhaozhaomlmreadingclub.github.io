8bf45f46f4e9ab52ba717a826a247583baa2f0a3
backrooms-level0 <test@runoob.com>
2024-08-25 23:11:50 +0800

    fixed main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ function homePage() {
     window.location.href = url
 }
 function resource() {
-    window.location.href = url + "resource.html"
+    window.location.href = url + "/resource.html"
 }
 function report() {
-    window.location.href = url + "report.html"
+    window.location.href = url + "/report.html"
 }
 function login() {
-    window.location.href = url + "login.html"
+    window.location.href = url + "/login.html"
 }
